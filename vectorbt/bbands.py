98ac6a29d903d3c3470b410bb7ca4e101a628205
gary0925 <yragyrag7@gmail.com>
2022-11-11 14:49:05 +0800

    add

--- vectorbt/bbands.py
+++ vectorbt/bbands.py
@@ -19,7 +19,7 @@ data['entry_short'] = np.where(data['close'] > data['upper'] ,True, False)
 data['exit_short'] = np.where(data['entry_short'].shift(10) ,True, False)  
 
 
-
+# change
 
 pf = vbt.Portfolio.from_signals(
     sl_stop = 0.01,  #i'm not sure about this usage
